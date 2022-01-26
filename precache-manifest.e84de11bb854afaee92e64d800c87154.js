c7b55cc3063c705ed7fff8a3bb4595b1e942c1a0
dahao <18570088982@163.com>
2022-01-26 16:44:37 +0800

    deploy

---
+++ precache-manifest.e84de11bb854afaee92e64d800c87154.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19bdc531de3eb8c350e",
+    "url": "/save-money-website/css/app.13433019.css"
+  },
+  {
+    "revision": "c60803045977d8955f26",
+    "url": "/save-money-website/css/chunk-vendors.207892dc.css"
+  },
+  {
+    "revision": "f440348189cd2e5f7e995fa74e13bdc6",
+    "url": "/save-money-website/img/404error.f4403481.png"
+  },
+  {
+    "revision": "54241cfda7af63a86b040ab703f28d95",
+    "url": "/save-money-website/index.html"
+  },
+  {
+    "revision": "d19bdc531de3eb8c350e",
+    "url": "/save-money-website/js/app.7d69c21f.js"
+  },
+  {
+    "revision": "c60803045977d8955f26",
+    "url": "/save-money-website/js/chunk-vendors.dac78352.js"
+  },
+  {
+    "revision": "2389b9051ff3dfa899176100d61983f5",
+    "url": "/save-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/save-money-website/robots.txt"
+  }
+]);
